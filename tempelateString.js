e144f96bedd98db6233a40bc4841f40c97be7960
shahidnsu <shahid.khan@northsouth.edu>
2019-03-04 18:55:54 +0600

    learnabout template string

---
+++ tempelateString.js
@@ -0,0 +1,9 @@
+let name = 'Shahid';
+//To put a varivable in tempelate string we use the syntax ${varivable name} 
+// syntax is (`things we want to print`)
+console.log(`Hi my name is ${name}!`);
+
+// more example 
+
+let age = 24;
+console.log(`Hi my name is ${name} . I am ${age} years old `)
