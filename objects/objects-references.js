dbf7cf037bf9688b31a7c5d7a6e0c5ab6b11c489
shahidnsu <shahid.khan@northsouth.edu>
2019-03-04 21:20:31 +0600

    learn about object-reference

---
+++ objects/objects-references.js
@@ -0,0 +1,16 @@
+let myAccount =  {
+    name : 'Andrew Mead',
+    expenses : 0,
+    income   : 0
+}
+let addExpenses  = function (account , amount){
+    account = {
+
+    }
+
+    //account.expenses  = account.expenses + amount ;
+    console.log(account);
+  
+}
+let account_deatils = addExpenses(myAccount, 2.50);
+console.log(myAccount);
